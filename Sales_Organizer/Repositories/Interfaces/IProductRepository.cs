783b94796a8713f0d0a59b864b242ac53f9bd77b
Niteesh Yarra <niteeshyarra@gmail.com>
2020-03-13 11:26:51 -0400

    Added ProductRepo

--- Sales_Organizer/Repositories/Interfaces/IProductRepository.cs
+++ Sales_Organizer/Repositories/Interfaces/IProductRepository.cs
@@ -10,7 +10,7 @@ namespace Sales_Organizer.Repositories.Interfaces
     {
         void AddProduct(Product product);
         Product GetProduct(int id);
-        void UpdateProduct(int id);
+        void UpdateProduct(Product product);
         void DeleteProduct(int id);
         IEnumerable<Product> GetAllProducts();
     }
